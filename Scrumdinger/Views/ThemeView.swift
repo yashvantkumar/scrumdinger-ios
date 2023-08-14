60013ce33bbc862a861bd6f395af57a1443b5549
Yashwant Shirol <yshirol94@gmail.com>
2023-08-14 11:01:19 +0530

    Added colour scheme

--- Scrumdinger/Views/ThemeView.swift
+++ Scrumdinger/Views/ThemeView.swift
@@ -11,7 +11,11 @@ struct ThemeView: View {
     let theme: Theme
     var body: some View {
         Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
             .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
     }
 }
 
